20d797c7b2cfec57d03e9f8219b6f2c3e08fae49
Ran <43445785+RanCraftPlayz@users.noreply.github.com>
2022-04-02 17:11:13 +0600

    Oops

--- src/main/java/io/github/ran/minecraft_mappings/SpigotMapper.java
+++ src/main/java/io/github/ran/minecraft_mappings/SpigotMapper.java
@@ -82,6 +82,7 @@ public class SpigotMapper {
             mapUtil.makeFieldMaps(new String(outputStream.toByteArray()), mappingFile, true);
         } else {
             mappingFile.createNewFile();
+            Files.copy(new File(buildDataDir, "mappings/" + versionInfo.memberMappings).toPath(), mappingFile.toPath());
         }
         Files.copy(classMappings.toPath(), clazzMappingFile.toPath());
         System.out.println("Successfully fetched spigot mappings!");
